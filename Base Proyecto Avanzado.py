b77d2d5d52c8918d45a06068c11aa402c6f6092c
Coolgolf1 <coolgolfmaster@gmail.com>
2023-11-10 11:59:38 +0100

    cambios

--- Base Proyecto Avanzado.py
+++ Base Proyecto Avanzado.py
@@ -1,16 +1,6 @@
-import os
-import binascii
-import base64
-from Crypto.Cipher import AES
-from Crypto.Cipher import DES
-from Crypto.Random import get_random_bytes
-from Crypto.Util.Padding import pad, unpad
-from Crypto.Protocol.KDF import PBKDF2
-from Crypto.Hash import SHA256
-from Crypto.PublicKey import RSA, ECC
-from Crypto.Cipher import PKCS1_OAEP
-from funciones_maquinas import *
 from funciones_ct_gn import *
+from funciones_maquinas import *
+from funciones_lecciones import *
 
 def main_menu():
     while True:
@@ -52,56 +42,6 @@ def maquinas_menu():
             maquinas_cifrados_asimetricos()
         elif maquinas_choice == "5":
             return
-
-
-def maquinas_cifrados_clasicos():
-    while True:
-        clear_terminal()
-        maquinas_cifrados_clasicos_choice = input(
-            "========= Máquinas ==========\n===== Cifrados Clásicos =====\n1. Cifrado César\n2. Cifrado Vigènere\n3. Cifrado Rail Fence\n4. Atrás\n\nIntroduce tu opción: ")
-        clear_terminal()
-        if maquinas_cifrados_clasicos_choice == "1":
-            caesar_cipher()
-        elif maquinas_cifrados_clasicos_choice == "2":
-            vigenere_cipher()
-        elif maquinas_cifrados_clasicos_choice == "3":
-            rail_fence_cipher()
-        elif maquinas_cifrados_clasicos_choice == "4":
-            return
-        else:
-            input("Error. No es una opción correcta. Pulsa enter para continuar.")
-
-
-def maquinas_cifrados_simetricos():
-    while True:
-        clear_terminal()
-        maquinas_cifrados_simetricos_choice = input(
-            "========== Máquinas ===========\n===== Cifrados Simétricos =====\n1. Cifrado AES-56\n2. Cifrado DES\n3. Atrás\n\nIntroduce tu opción: ")
-        clear_terminal()
-        if maquinas_cifrados_simetricos_choice == "1":
-            AES_cipher()
-        elif maquinas_cifrados_simetricos_choice == "2":
-            DES_cipher()
-        elif maquinas_cifrados_simetricos_choice == "3":
-            return
-        else:
-            input("Error. No es una opción correcta. Pulsa enter para continuar.")
-
-
-def maquinas_cifrados_asimetricos():
-    while True:
-        clear_terminal()
-        maquinas_cifrados_simetricos_choice = input(
-            "=========== Máquinas ===========\n===== Cifrados Asimétricos =====\n1. Cifrado RSA\n2. Cifrado Curva Elíptica\n3. Atrás\n\nIntroduce tu opción: ")
-        clear_terminal()
-        if maquinas_cifrados_simetricos_choice == "1":
-            RSA_cipher()
-        elif maquinas_cifrados_simetricos_choice == "2":
-            ECC_cipher()
-        elif maquinas_cifrados_simetricos_choice == "3":
-            return
-        else:
-            input("Error. No es una opción correcta. Pulsa enter para continuar.")
 
 
 def exit_programa():
--- funciones_maquinas.py
+++ funciones_maquinas.py
@@ -9,6 +9,58 @@ from Crypto.Protocol.KDF import PBKDF2
 from Crypto.Hash import SHA256
 from Crypto.PublicKey import RSA, ECC
 from Crypto.Cipher import PKCS1_OAEP
+from funciones_ct_gn import *
+
+
+def maquinas_cifrados_clasicos():
+    while True:
+        clear_terminal()
+        maquinas_cifrados_clasicos_choice = input(
+            "========= Máquinas ==========\n===== Cifrados Clásicos =====\n1. Cifrado César\n2. Cifrado Vigènere\n3. Cifrado Rail Fence\n4. Atrás\n\nIntroduce tu opción: ")
+        clear_terminal()
+        if maquinas_cifrados_clasicos_choice == "1":
+            caesar_cipher()
+        elif maquinas_cifrados_clasicos_choice == "2":
+            vigenere_cipher()
+        elif maquinas_cifrados_clasicos_choice == "3":
+            rail_fence_cipher()
+        elif maquinas_cifrados_clasicos_choice == "4":
+            return
+        else:
+            input("Error. No es una opción correcta. Pulsa enter para continuar.")
+
+
+def maquinas_cifrados_simetricos():
+    while True:
+        clear_terminal()
+        maquinas_cifrados_simetricos_choice = input(
+            "========== Máquinas ===========\n===== Cifrados Simétricos =====\n1. Cifrado AES-56\n2. Cifrado DES\n3. Atrás\n\nIntroduce tu opción: ")
+        clear_terminal()
+        if maquinas_cifrados_simetricos_choice == "1":
+            AES_cipher()
+        elif maquinas_cifrados_simetricos_choice == "2":
+            DES_cipher()
+        elif maquinas_cifrados_simetricos_choice == "3":
+            return
+        else:
+            input("Error. No es una opción correcta. Pulsa enter para continuar.")
+
+
+def maquinas_cifrados_asimetricos():
+    while True:
+        clear_terminal()
+        maquinas_cifrados_simetricos_choice = input(
+            "=========== Máquinas ===========\n===== Cifrados Asimétricos =====\n1. Cifrado RSA\n2. Cifrado Curva Elíptica\n3. Atrás\n\nIntroduce tu opción: ")
+        clear_terminal()
+        if maquinas_cifrados_simetricos_choice == "1":
+            RSA_cipher()
+        elif maquinas_cifrados_simetricos_choice == "2":
+            ECC_cipher()
+        elif maquinas_cifrados_simetricos_choice == "3":
+            return
+        else:
+            input("Error. No es una opción correcta. Pulsa enter para continuar.")
+
 
 # Este es el proceso de encripción/desencripción del Cifrado César
 def caesar_cipher_process(input_text_caesar, caesar_key, modo):
