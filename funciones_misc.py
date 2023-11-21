bbc100ddb4985f4e7bc9186fa06734ee9a0be147
Coolgolf1 <coolgolfmaster@gmail.com>
2023-11-21 09:38:48 +0100

    links

--- funciones_misc.py
+++ funciones_misc.py
@@ -25,27 +25,32 @@ def get_name():
 
 
 def sigue_aprendiendo():
-    clear_terminal()
-    print("""Las lecciones de los cifrados avanzados en este juego son bastante simples
-Por ello, recomiendo, que si quieres aprender el funcionamiento completo de cada cifrado, que uses estos links.""")
-    links_choice = input("¿Qué cifrado quieres mirar?\n1. AES\n2. DES\n3. RSA\n4. ECC\n5. Atrás\n\nIntroduce tu opción: ")
-    clear_terminal()
-    if links_choice == "1":
-        print("\nCryptoHack AES (INTERMEDIO): https://cryptohack.org/courses/symmetric/")
-        input()
-    elif links_choice == "2":
-        print("\nGeeksForGeeks DES (ANTIGUO): https://www.geeksforgeeks.org/data-encryption-standard-des-set-1/")
-        input()
-    elif links_choice == "3":
-        print("\nCryptoHack RSA (INTERMEDIO): https://cryptohack.org/courses/public-key/")
-        input()
-    elif links_choice == "4":
-        print("\CryptoHack ECC (DIFÍCIL): https://cryptohack.org/courses/elliptic/")
-        input()
-    elif links_choice == "5":
-        return
-    else:
-        input("Error. No es una opción correcta. Pulsa enter para continuar.")
+    links_choice = 0
+    while links_choice != "6":
+        clear_terminal()
+        print("""Las lecciones de los cifrados avanzados en este juego son bastante simples
+    Por ello, recomiendo, que si quieres aprender el funcionamiento completo de cada cifrado, que uses estos links.""")
+        links_choice = input("¿Qué cifrado quieres mirar?\n1. AES\n2. DES\n3. RSA\n4. ECC\n5. Atrás\n\nIntroduce tu opción: ")
+        clear_terminal()
+        if links_choice == "1":
+            print("CrypTool (Aprende Visualmente): https://www.cryptool.org/en/")
+            input()
+        elif links_choice == "2":
+            print("\nCryptoHack AES (INTERMEDIO): https://cryptohack.org/courses/symmetric/")
+            input()
+        elif links_choice == "3":
+            print("\nGeeksForGeeks DES (ANTIGUO): https://www.geeksforgeeks.org/data-encryption-standard-des-set-1/")
+            input()
+        elif links_choice == "4":
+            print("\nCryptoHack RSA (INTERMEDIO): https://cryptohack.org/courses/public-key/")
+            input()
+        elif links_choice == "5":
+            print("\CryptoHack ECC (DIFÍCIL): https://cryptohack.org/courses/elliptic/")
+            input()
+        elif links_choice == "6":
+            return
+        else:
+            input("Error. No es una opción correcta. Pulsa enter para continuar.")
 
 
 def exit_programa():
