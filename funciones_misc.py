21b9b763d81572151ecb4879af206c2242d54682
Coolgolf1 <coolgolfmaster@gmail.com>
2023-11-24 11:57:06 +0100

    format

--- funciones_misc.py
+++ funciones_misc.py
@@ -1,8 +1,7 @@
 import os
 
+
 # Esta función limpia la terminal por estética
-
-
 def clear_terminal():
     os.system("cls")
     print("""\n\n             _|_|_|_|                                                      _|      _|_|_|_|        _|  
